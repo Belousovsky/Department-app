8933a1ab7a1c2fc48489dba3efe59c1414aa49d6
Belousovsky <belousov.dany@yandex.ru>
2019-07-19 18:29:56 +0300

    made the main page following the example of the first react app

---
+++ src/components/News.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Article } from './Article' // идти в components не нужно, так как мы уже в этой директории
+
+
+class News extends React.Component {
+
+    state = { // создали состояние
+        filteredNews: this.props.data,
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        let nextFilteredNews = [...props.data] // было nextProps - переименовали
+
+        nextFilteredNews.forEach((item, index) => {
+            if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+                item.bigText = 'СПАМ'
+            }
+        })
+
+        return { // возвращаем новое состояние
+            filteredNews: nextFilteredNews,
+        }
+    }
+
+    renderNews = () => {
+        const { filteredNews } = this.state // используем состояние
+        let newsTemplate = null
+
+        if (filteredNews.length) { // везде data заменена на filteredNews
+            newsTemplate = filteredNews.map(function(item) {
+                return <Article key={item.id} data={item} />
+            })
+        } else {
+            newsTemplate = <p>К сожалению новостей нет</p>
+        }
+
+        return newsTemplate
+    }
+    render() {
+        const { filteredNews } = this.state // аналогично, используем состояние
+
+        return (
+            <div className="news">
+                {this.renderNews()}
+                {filteredNews.length ? (
+                    <strong className={'news__count'}>
+                        Всего новостей: {filteredNews.length}
+                    </strong>
+                ) : null}
+            </div>
+        )
+    }
+}
+
+News.propTypes = {
+    data: PropTypes.array.isRequired
+}
+
+export { News }
